090238e035d76936a4005d010189ad33c066f2a0
gbamparop <georgios.bamparopoulos@elastic.co>
2023-02-06 16:53:34 +0000

    APM custom controls

--- x-pack/plugins/apm/public/components/shared/search_bar.tsx
+++ x-pack/plugins/apm/public/components/shared/search_bar.tsx
@@ -13,6 +13,13 @@ import {
   EuiSpacer,
 } from '@elastic/eui';
 import React from 'react';
+import { LazyControlGroupRenderer } from '@kbn/controls-plugin/public';
+import { ViewMode } from '@kbn/embeddable-plugin/public';
+import { withSuspense } from '@kbn/shared-ux-utility';
+import { EuiLoadingSpinner } from '@elastic/eui';
+import { getPhraseFilterValue } from '@kbn/es-query';
+import { useHistory } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
 import { isMobileAgentName } from '../../../common/agent_name';
 import { useApmServiceContext } from '../../context/apm_service/use_apm_service_context';
 import { useBreakpoints } from '../../hooks/use_breakpoints';
@@ -20,6 +27,9 @@ import { ApmDatePicker } from './date_picker/apm_date_picker';
 import { KueryBar } from './kuery_bar';
 import { TimeComparison } from './time_comparison';
 import { TransactionTypeSelect } from './transaction_type_select';
+import { APM_STATIC_DATA_VIEW_ID } from '../../../common/data_view_constants';
+import * as urlHelpers from './links/url_helpers';
+import { TRANSACTION_TYPE } from '../../../common/es_fields/apm';
 
 interface Props {
   hidden?: boolean;
@@ -29,6 +39,11 @@ interface Props {
   kueryBarPlaceholder?: string;
   kueryBarBoolFilter?: QueryDslQueryContainer[];
 }
+
+const ControlGroupRenderer = withSuspense(
+  LazyControlGroupRenderer,
+  <EuiLoadingSpinner />
+);
 
 export function SearchBar({
   hidden = false,
@@ -42,6 +57,7 @@ export function SearchBar({
   const isMobileAgent = isMobileAgentName(agentName);
 
   const { isSmall, isMedium, isLarge, isXl, isXXL, isXXXL } = useBreakpoints();
+  const history = useHistory();
 
   if (hidden) {
     return null;
@@ -66,6 +82,36 @@ export function SearchBar({
             {showTransactionTypeSelector && (
               <EuiFlexItem grow={false}>
                 <TransactionTypeSelect />
+                <ControlGroupRenderer
+                  getInitialInput={async (initialInput, builder) => {
+                    await builder.addDataControlFromField(initialInput, {
+                      dataViewId: APM_STATIC_DATA_VIEW_ID,
+                      title: i18n.translate(
+                        'xpack.apm.control.transactionType',
+                        {
+                          defaultMessage: 'Transaction type',
+                        }
+                      ),
+                      fieldName: TRANSACTION_TYPE,
+                      width: 'medium',
+                      grow: true,
+                    });
+                    return {
+                      ...initialInput,
+                      viewMode: ViewMode.VIEW,
+                    };
+                  }}
+                  onLoadComplete={(controlGroup) => {
+                    controlGroup.onFiltersPublished$.subscribe({
+                      next: (result) => {
+                        const value = getPhraseFilterValue(result[0]);
+                        urlHelpers.push(history, {
+                          query: { transactionType: value },
+                        });
+                      },
+                    });
+                  }}
+                />
               </EuiFlexItem>
             )}
 
